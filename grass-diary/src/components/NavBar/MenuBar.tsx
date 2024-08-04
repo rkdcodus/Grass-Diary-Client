1cfae8aa73de945f8abc70468dc984863073e3fa
rkdcodus <rcd7275@naver.com>
2024-08-04 21:56:14 +0900

    ♻ refactor: Menus 컴포넌트 적용

--- grass-diary/src/components/NavBar/MenuBar.tsx
+++ grass-diary/src/components/NavBar/MenuBar.tsx
@@ -1,6 +1,3 @@
-import styled from 'styled-components';
-import { semantic } from '@styles/semantic';
-
 import person from '@svg/person.svg';
 import setting from '@svg/settings.svg';
 import logout from '@svg/logout.svg';
@@ -9,13 +6,14 @@ import Menu from '@components/Button/Menu';
 
 import useLogout from '@hooks/useLogout';
 import { useQueryClient } from '@tanstack/react-query';
+import Menus from '@components/Button/Menus';
 
-interface IMenuBarProps {
+interface MenuBarProps {
   toggle: boolean;
   headerRef: React.RefObject<HTMLDivElement>;
 }
 
-const MenuBar = ({ toggle, headerRef }: IMenuBarProps) => {
+const MenuBar = ({ toggle, headerRef }: MenuBarProps) => {
   const clearAuth = useLogout();
   const queryClient = useQueryClient();
   const handleLogout = () => {
@@ -28,36 +26,15 @@ const MenuBar = ({ toggle, headerRef }: IMenuBarProps) => {
   };
 
   return (
-    <MenusContainer ref={headerRef} toggle={toggle}>
-      <Menu link={'/mypage'} text={'마이페이지'} svg={person} line={4} />
-      <Menu link={'/setting'} text={'설정'} svg={setting} />
-      <Menu onClick={handleLogout} text={'로그아웃'} svg={logout} />
-      <Menu onClick={deleteAccount} text={'탈퇴'} svg={person_remove} />
-    </MenusContainer>
+    <div ref={headerRef}>
+      <Menus toggle={toggle}>
+        <Menu link={'/mypage'} text={'마이페이지'} svg={person} line={4} />
+        <Menu link={'/setting'} text={'설정'} svg={setting} />
+        <Menu onClick={handleLogout} text={'로그아웃'} svg={logout} />
+        <Menu onClick={deleteAccount} text={'탈퇴'} svg={person_remove} />
+      </Menus>
+    </div>
   );
 };
 
 export default MenuBar;
-
-const MenusContainer = styled.div<{ toggle: boolean }>`
-  display: flex;
-  width: 160px;
-  height: ${props => (props.toggle ? 'auto' : '0px')};
-  padding: var(--gap-empty, 0px);
-  flex-direction: column;
-  align-items: flex-start;
-  gap: var(--gap-empty, 0px);
-
-  border-radius: var(--radius-md, 16px);
-  opacity: var(--opacity-visible, 1);
-  background: ${semantic.light.bg.solid.normal};
-
-  /* shadow/floated */
-  box-shadow: 0px 0px 2px 0px rgba(0, 0, 0, 0.06),
-    0px 2px 4px 0px rgba(0, 0, 0, 0.06), 0px 4px 8px 0px rgba(0, 0, 0, 0.13);
-
-  position: absolute;
-  transform: translate(-92px, 11px);
-  overflow: hidden;
-  z-index: 998;
-`;
