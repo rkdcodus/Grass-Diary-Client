c69bc73d087ab9d3cd5abd3420c49c04fd472e45
rkdcodus <rcd7275@naver.com>
2024-08-04 20:05:47 +0900

    ✨feat: Menu 컴포넌트 생성

---
+++ grass-diary/src/components/Button/Menu.tsx
@@ -0,0 +1,70 @@
+import styled from 'styled-components';
+import { semantic } from '@styles/semantic';
+import { TYPO } from '@styles/typo';
+
+import Interaction1 from '@components/Interactions/Interaction1';
+import ButtonContainer from '@components/Button/ButtonContainer';
+
+import { Link } from 'react-router-dom';
+
+interface MenuProps {
+  text: string;
+  svg: string;
+  link?: string;
+  onClick?: () => void;
+  line?: number;
+}
+
+const Menu = ({ link, onClick, text, svg, line }: MenuProps) => {
+  return (
+    <>
+      <Link to={link || ''}>
+        <ButtonContainer>
+          <Interaction1 onClick={onClick} />
+          <MenuContainer>
+            <MenuStr>{text}</MenuStr>
+            <MenuImg src={svg} alt={text} />
+          </MenuContainer>
+        </ButtonContainer>
+      </Link>
+      <Line height={line} />
+    </>
+  );
+};
+
+export default Menu;
+
+const MenuContainer = styled.div`
+  width: 160px;
+  display: flex;
+  padding: var(--gap-md, 16px) var(--gap-lg, 20px);
+  align-items: center;
+  gap: var(--gap-md, 16px);
+  align-self: stretch;
+
+  border-radius: var(--radius-empty, 0px);
+  opacity: var(--opacity-visible, 1);
+`;
+
+const Line = styled.div<{ height: number | undefined }>`
+  height: ${props => props.height || 1}px;
+  align-self: stretch;
+  background: ${semantic.light.border.transparent.assistive};
+`;
+
+const MenuStr = styled.p`
+  flex: 1 0 0;
+  color: ${semantic.light.object.solid.normal};
+  ${TYPO.label2};
+
+  opacity: var(--opacity-visible, 1);
+`;
+
+const MenuImg = styled.img`
+  display: flex;
+  justify-content: center;
+  text-align: center;
+  width: 18px;
+  height: 18px;
+  opacity: var(--opacity-visible, 1);
+`;
