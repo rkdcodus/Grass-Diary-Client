86d2a601252c5a2461438059f56672cb924845d0
rkdcodus <rcd7275@naver.com>
2024-08-12 15:13:48 +0900

    🔨fix: Interaction 컴포넌트 수정
    불필요한 코드 삭제 및 Interaction 4 컴포넌트 명도값 1->0으로 올바르게 수정

--- grass-diary/src/components/Interactions/Interaction3.tsx
+++ grass-diary/src/components/Interactions/Interaction3.tsx
@@ -25,23 +25,19 @@ const Interaction = styled.button<InteractionStyleProps>`
   border-top-right-radius: ${props => props.$topRadius || 0}px;
   border-bottom-left-radius: ${props => props.$bottomRadius || 0}px;
   border-bottom-right-radius: ${props => props.$bottomRadius || 0}px;
-  // opacity: var(--opacity-subtlest, 0);
   background: rgba(0, 137, 90, 0);
   cursor: pointer;
 
   &:hover {
-    // opacity: var(--opacity-subtlest, 0.05);
     background: rgba(0, 137, 90, 0.05);
   }
 
   &:active {
-    // opacity: var(--opacity-subtler, 0.08);
     background: rgba(0, 137, 90, 0.08);
   }
 
   &:focus {
     border: var(--stroke-thick, 2px) solid
       ${semantic.light.interactive.solid.focused};
-    // opacity: var(--opacity-visible, 1);
   }
 `;
