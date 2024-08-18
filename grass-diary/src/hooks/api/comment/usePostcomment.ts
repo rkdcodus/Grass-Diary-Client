2238084de3f4905292d016dd75ae18c8d32d2059
rkdcodus <rcd7275@naver.com>
2024-08-18 14:32:10 +0900

    🔥remove: 불필요한 파일 삭제

--- grass-diary/src/hooks/api/comment/usePostcomment.ts
+++ grass-diary/src/hooks/api/comment/usePostcomment.ts
@@ -1,3 +1,4 @@
+import { END_POINT } from '@constants/api';
 import API from '@services/index';
 import { useMutation, useQueryClient } from '@tanstack/react-query';
 
@@ -5,38 +6,12 @@ export const usePostComment = (diaryId: Id) => {
   const queryClient = useQueryClient();
   return useMutation({
     mutationFn: async (request: PostRequest) =>
-      API.post(`/comment/${diaryId}`, request),
-    onMutate: async request => {
-      await queryClient.cancelQueries({ queryKey: ['comment', diaryId] });
-
-      // 업데이트 이전 정보 저장.
-      const priviousData = queryClient.getQueryData(['comment', diaryId]);
-      const newRequest = {
-        commentId: 999,
-        memberId: request.memberId,
-        content: request.content,
-        deleted: false,
-        createdDate: null,
-        createdAt: null,
-        depth: 0,
-        childComments: [],
-      };
-
-      queryClient.setQueryData(
-        ['comment', diaryId],
-        (prev: CommentResponse[]) => [...prev, newRequest],
-      );
-
-      return { priviousData };
-    },
-    onError(error, data, context) {
-      if (context) {
-        queryClient.setQueryData(['comment', diaryId], context.priviousData);
-      }
-      console.error('댓글 post 실패 ', error);
-    },
-    onSettled() {
+      API.post(END_POINT.COMMENT(diaryId), request),
+    onSuccess() {
       queryClient.invalidateQueries({ queryKey: ['comment', diaryId] });
+    },
+    onError(error) {
+      console.error(error.response.data.description);
     },
   });
 };
