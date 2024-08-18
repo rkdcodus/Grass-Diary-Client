e4b882533b02ca3864b78b4cfc32e9bd88f40126
rkdcodus <rcd7275@naver.com>
2024-08-18 14:24:14 +0900

    🔨fix: 댓글 post 낙관적 업데이트 대신 재조회로 변경

--- grass-diary/src/hooks/api/comment/usePostComment.ts
+++ grass-diary/src/hooks/api/comment/usePostComment.ts
@@ -6,37 +6,11 @@ export const usePostComment = (diaryId: Id) => {
   return useMutation({
     mutationFn: async (request: PostRequest) =>
       API.post(`/comment/${diaryId}`, request),
-    onMutate: async request => {
-      await queryClient.cancelQueries({ queryKey: ['comment', diaryId] });
-
-      // 업데이트 이전 정보 저장.
-      const priviousData = queryClient.getQueryData(['comment', diaryId]);
-      const newRequest = {
-        commentId: 999,
-        memberId: request.memberId,
-        content: request.content,
-        deleted: false,
-        createdDate: null,
-        createdAt: null,
-        depth: 0,
-        childComments: [],
-      };
-
-      queryClient.setQueryData(
-        ['comment', diaryId],
-        (prev: CommentResponse[]) => [...prev, newRequest],
-      );
-
-      return { priviousData };
-    },
-    onError(error, data, context) {
-      if (context) {
-        queryClient.setQueryData(['comment', diaryId], context.priviousData);
-      }
-      console.error('댓글 post 실패 ', error);
-    },
-    onSettled() {
+    onSuccess() {
       queryClient.invalidateQueries({ queryKey: ['comment', diaryId] });
+    },
+    onError(error) {
+      console.error(error.response.data.description);
     },
   });
 };
