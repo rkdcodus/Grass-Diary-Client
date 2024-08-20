0366b081e50916a246fceaa0d4cd7da96beb20bb
rkdcodus <rcd7275@naver.com>
2024-08-20 22:45:11 +0900

    🔥 remove: 불필요한 코드 삭제

--- grass-diary/src/router.tsx
+++ grass-diary/src/router.tsx
@@ -1,6 +1,5 @@
 import { createBrowserRouter } from 'react-router-dom';
 import { ProtectedRoute } from '@components/index';
-import { lazy } from 'react';
 
 import {
   Intro,
@@ -14,19 +13,6 @@ import {
   NonExistentDiary,
   RewardPage,
 } from '@pages/index';
-
-// const Intro = lazy(() => import('@pages/Intro/Intro'));
-// const Main = lazy(() => import('@pages/Main/Main'));
-// const CreateDiary = lazy(() => import('@pages/CreateDiary/CreateDiary'));
-// const EditDiary = lazy(() => import('@pages/EditDiary/EditDiary'));
-// const DiaryDetail = lazy(() => import('@pages/DiaryDetail/DiaryDetail'));
-// const Share = lazy(() => import('@pages/Share/Share'));
-// const Setting = lazy(() => import('@pages/Setting/Setting'));
-// const MyPage = lazy(() => import('@pages/MyPage/MyPage'));
-// const NonExistentDiary = lazy(
-//   () => import('@pages/DiaryDetail/NonExistentDiary'),
-// );
-// const RewardPage = lazy(() => import('@pages/RewardPage/RewardPage'));
 
 const router = createBrowserRouter([
   {
