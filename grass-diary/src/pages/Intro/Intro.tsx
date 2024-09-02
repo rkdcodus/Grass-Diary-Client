09e53eadd6ae82c3c2ff20566e2d732b1cea495f
ccconac <qwerpoiu91@naver.com>
2024-09-02 20:40:30 +0900

    :lipstick: design: 인트로 페이지 UI 구현

--- grass-diary/src/pages/Intro/Intro.tsx
+++ grass-diary/src/pages/Intro/Intro.tsx
@@ -1,37 +1,71 @@
-import { Container, Header } from '@components/index';
-import {
-  ServiceMain,
-  MainDesc,
-  SecondDesc,
-  StartContent,
-} from './introComponents';
-import ReactFullpage from '@fullpage/react-fullpage';
+import * as S from '../../styles/Intro/styles';
+import { Divider } from '@components/index';
+import { INTRO_MESSAGES } from '@constants/message';
 
 const Intro = () => {
   return (
-    <>
-      <ReactFullpage
-        scrollingSpeed={1000}
-        render={({ state }) => {
-          return (
-            <ReactFullpage.Wrapper>
-              <div className="section" style={{ backgroundColor: '#FEFEFE' }}>
-                <ServiceMain />
-              </div>
-              <div className="section" style={{ backgroundColor: '#F9FFF7' }}>
-                <MainDesc />
-              </div>
-              <div className="section" style={{ backgroundColor: '#F0FFEA' }}>
-                <SecondDesc />
-              </div>
-              <div className="section" style={{ backgroundColor: '#E1FFD7' }}>
-                <StartContent />
-              </div>
-            </ReactFullpage.Wrapper>
-          );
-        }}
-      />
-    </>
+    <S.IntroContainer>
+      <S.CommonSection>
+        <S.MainBox>
+          <S.FirstTitleContainer>
+            <S.LogoImg src="/assets/icons/logo.png" />
+            <S.MainTitle>{INTRO_MESSAGES.firstSection.serviceName}</S.MainTitle>
+          </S.FirstTitleContainer>
+          <S.MainIntrouctionText>
+            {INTRO_MESSAGES.firstSection.serviceIntroduction}
+          </S.MainIntrouctionText>
+          <Divider />
+          <S.LoginButtonContainer>
+            <S.LoginButton>
+              <S.ButtonText>{INTRO_MESSAGES.writeDiary}</S.ButtonText>
+              <img src="/assets/icons/button-solid-chevron-right.svg" />
+            </S.LoginButton>
+          </S.LoginButtonContainer>
+        </S.MainBox>
+      </S.CommonSection>
+      <S.ScrollMessageContainer>
+        <S.ScrollText>{INTRO_MESSAGES.firstSection.scrollMessage}</S.ScrollText>
+        <S.ScrollImg src="/assets/icons/expand-more.svg" />
+      </S.ScrollMessageContainer>
+      <S.CommonSection>
+        <S.CommonArticle>
+          <S.CommonTitleContainer>
+            <S.CommonTitle>
+              {INTRO_MESSAGES.secondSection.secondTitle}
+            </S.CommonTitle>
+            <S.CommonIntroductionText>
+              {INTRO_MESSAGES.secondSection.secondIntroduction}
+            </S.CommonIntroductionText>
+          </S.CommonTitleContainer>
+        </S.CommonArticle>
+      </S.CommonSection>
+      <S.CommonSection>
+        <S.CommonArticle>
+          <S.CommonTitleContainer>
+            <S.CommonTitle>
+              {INTRO_MESSAGES.thirdSection.thirdTitle}
+            </S.CommonTitle>
+            <S.CommonIntroductionText>
+              {INTRO_MESSAGES.thirdSection.thirdIntroduction}
+            </S.CommonIntroductionText>
+          </S.CommonTitleContainer>
+        </S.CommonArticle>
+      </S.CommonSection>
+      <S.CommonSection>
+        <S.MainBox>
+          <S.MainIntrouctionText>
+            {INTRO_MESSAGES.lastSection.startMessage}
+          </S.MainIntrouctionText>
+          <Divider />
+          <S.LoginButtonContainer>
+            <S.LoginButton>
+              <S.ButtonText>{INTRO_MESSAGES.writeDiary}</S.ButtonText>
+              <img src="/assets/icons/button-solid-chevron-right.svg" />
+            </S.LoginButton>
+          </S.LoginButtonContainer>
+        </S.MainBox>
+      </S.CommonSection>
+    </S.IntroContainer>
   );
 };
 
