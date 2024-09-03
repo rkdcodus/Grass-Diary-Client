ac06d3e608f71ffb9181872fffcf7130ba671830
ccconac <qwerpoiu91@naver.com>
2024-09-04 01:33:01 +0900

    :hammer: fix: divider component props 추가

--- grass-diary/src/components/Divider.tsx
+++ grass-diary/src/components/Divider.tsx
@@ -1,15 +1,26 @@
 import { semantic } from '@styles/semantic';
 import styled from 'styled-components';
 
-const Divider = () => {
-  return <DividerLine />;
+interface IDividerProps {
+  width?: string;
+  color?: string;
+}
+
+const Divider = ({ width, color }: IDividerProps) => {
+  return <DividerLine $dividerWidth={width} $dividerColor={color} />;
 };
 
 export default Divider;
 
-const DividerLine = styled.div`
-  width: 20rem;
+const DividerLine = styled.div<{
+  $dividerColor?: string;
+  $dividerWidth?: string;
+}>`
+  width: ${({ $dividerWidth }) => $dividerWidth || '20rem'};
   height: 0.0625rem;
 
-  background: ${semantic.light.border.transparent.neutral};
+  background: ${({ $dividerColor }) =>
+    $dividerColor
+      ? $dividerColor
+      : `${semantic.light.border.transparent.neutral}`};
 `;
