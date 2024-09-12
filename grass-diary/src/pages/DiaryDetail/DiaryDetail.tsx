67f31ad14d51a5505e48b70ef2ef8f048bb5a01d
rkdcodus <rcd7275@naver.com>
2024-09-12 19:32:26 +0900

    🔨fix:  상세페이지 min-height  % -> dvh 단위 변경

--- grass-diary/src/pages/DiaryDetail/DiaryDetail.tsx
+++ grass-diary/src/pages/DiaryDetail/DiaryDetail.tsx
@@ -169,7 +169,7 @@ const Container = styled.div`
 
 const DiaryBox = styled.div`
   margin: auto;
-  min-height: 100%;
+  min-height: 100dvh;
   display: flex;
   padding: var(--gap-4xl, 3rem) var(--gap-9xl, 8.5rem) var(--gap-7xl, 4.5rem)
     var(--gap-9xl, 8.5rem);
