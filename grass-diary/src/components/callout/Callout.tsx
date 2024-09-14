59aaf1e36181eef4b2803a807ba3369fcb9a4b50
Yongho Kim <kyhcraft@gmail.com>
2024-09-15 07:51:29 +0900

    ♻️ refactor: Callout 컴포넌트 분리, 미디어 쿼리 수정

--- grass-diary/src/components/callout/Callout.tsx
+++ grass-diary/src/components/callout/Callout.tsx
@@ -1,6 +1,4 @@
-import styled from 'styled-components';
-import { semantic } from '@styles/semantic';
-import { TYPO } from '@styles/typo';
+import * as S from '@styles/components/callout/Callout.style';
 import { ReactComponent as Info } from '@svg/info.svg';
 
 type CalloutProps = {
@@ -9,30 +7,13 @@ type CalloutProps = {
 
 const Callout = ({ message }: CalloutProps) => {
   return (
-    <Container>
-      <Info />
-      <Text>{message}</Text>
-    </Container>
+    <S.Container>
+      <S.Space>
+        <Info />
+      </S.Space>
+      <S.Text>{message}</S.Text>
+    </S.Container>
   );
 };
 
-const Container = styled.div`
-  display: flex;
-  align-self: center;
-  padding: var(--gap-3xs, 0.375rem) var(--gap-sm, 0.75rem);
-  align-items: flex-start;
-  gap: var(--gap-2xs, 0.5rem);
-
-  border-radius: var(--radius-xs, 0.5rem);
-  border: var(--stroke-thin, 0.0625rem) solid
-    ${semantic.light.border.transparent.alternative};
-
-  background: ${semantic.light.fill.transparent.assistive};
-`;
-
-const Text = styled.p`
-  color: ${semantic.light.object.transparent.alternative};
-
-  ${TYPO.caption2}
-`;
 export default Callout;
