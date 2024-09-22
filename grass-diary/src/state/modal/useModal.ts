90790ccd109568a902cca715eb2fd75434d705cb
rkdcodus <rcd7275@naver.com>
2024-09-22 15:57:05 +0900

    ✨feat: 설정페이지 준비 중 모달 구현
    회원 탈퇴 버튼/ 이미지 업로드 및 삭제 버튼

--- grass-diary/src/state/modal/useModal.ts
+++ grass-diary/src/state/modal/useModal.ts
@@ -4,6 +4,7 @@ import { useModalActions } from './ModalStore';
 export const useModal = () => {
   const { setLogin, setActive, setSetting } = useModalActions();
   const { setResetActive, setButton1, setButton2 } = useModalButtonActions();
+
   const modal = (setting: Setting, button1?: Button1, button2?: Button2) => {
     setResetActive();
     setSetting(setting);
@@ -11,6 +12,7 @@ export const useModal = () => {
     if (button2) setButton2(button2);
     setActive(true);
   };
+
   const loginModal = () => {
     const setting = {
       title: '회원가입 및 로그인',
@@ -20,5 +22,6 @@ export const useModal = () => {
     setLogin(true);
     setSetting(setting);
   };
+
   return { modal, loginModal };
 };
