144895be4b50493fb69b7be89a1f193e51dc05e1
rkdcodus <rcd7275@naver.com>
2024-09-23 16:48:29 +0900

    🔥 remove: 사용하지 않는 코드 삭제

--- grass-diary/src/components/Layout/Layout.tsx
+++ grass-diary/src/components/Layout/Layout.tsx
@@ -2,19 +2,17 @@ import { Outlet } from 'react-router-dom';
 import Header from './Header';
 import Footer from './Footer';
 import { Toast, Modal, SnackBar } from '@components/index';
-import { ErrorBoundary } from 'react-error-boundary';
-import { ErrorPage } from '@pages/index';
 
 const Layout = () => {
   return (
-    <ErrorBoundary FallbackComponent={ErrorPage}>
+    <>
       <Header />
       <Outlet />
       <Toast />
       <Modal />
       <SnackBar />
       <Footer />
-    </ErrorBoundary>
+    </>
   );
 };
 
