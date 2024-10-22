b3f849aa144dba0e6e6647acf0c437c7ad1cce27
rkdcodus <rcd7275@naver.com>
2024-10-22 19:51:25 +0900

    ✨feat: authStore에 state 추가
    expDate, setExpDate 추가

--- grass-diary/src/state/auth/authStore.ts
+++ grass-diary/src/state/auth/authStore.ts
@@ -5,21 +5,25 @@ import { create } from 'zustand';
 interface Actions {
   setIsAuthenticated: (isAuthenticated: boolean) => void;
   setIsLoading: (isLoading: boolean) => void;
+  setExpDate: (expDate: Date) => void;
   handleCheckAuth: () => Promise<void>;
 }
 
 interface AuthState {
   isAuthenticated: boolean;
   isLoading: boolean;
+  expDate: Date | null;
   actions: Actions;
 }
 
 const useAuthStore = create<AuthState>(set => ({
   isAuthenticated: false,
   isLoading: true,
+  expDate: null,
   actions: {
     setIsAuthenticated: isAuthenticated => set({ isAuthenticated }),
     setIsLoading: isLoading => set({ isLoading }),
+    setExpDate: expDate => set({ expDate }),
     handleCheckAuth: async () => {
       try {
         const isLoggedIn: boolean = await checkAuth();
@@ -36,5 +40,6 @@ const useAuthStore = create<AuthState>(set => ({
 
 export const useIsAuthenticated = () =>
   useAuthStore(state => state.isAuthenticated);
-export const useIsLoding = () => useAuthStore(state => state.isLoading);
+export const useIsLoading = () => useAuthStore(state => state.isLoading);
+export const useAuthExpDate = () => useAuthStore(state => state.expDate);
 export const useAuthActions = () => useAuthStore(state => state.actions);
